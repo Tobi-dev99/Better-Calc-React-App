8744a31d006f51eee8803c519ffd7161386b7436
Tobi-dev99 <talmale.vastuk5566@gmail.com>
2020-07-25 20:44:28 +0530

    Updates

---
+++ precache-manifest.a94dcbaf3ca800f70dfd1551eb470178.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88309df144b3a2c81054f6b34db822b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2a5e493675729f38c59",
+    "url": "/static/css/main.acf08fbe.chunk.css"
+  },
+  {
+    "revision": "6473406d9a71010eb715",
+    "url": "/static/js/2.708489c4.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/static/js/2.708489c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2a5e493675729f38c59",
+    "url": "/static/js/main.3ca20ffa.chunk.js"
+  },
+  {
+    "revision": "75987c5cc4966cd66c10",
+    "url": "/static/js/runtime-main.82157934.js"
+  }
+]);
