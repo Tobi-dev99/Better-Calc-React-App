a6edd9039f665394e1301a489b6f9d5a3800973b
Tobi-dev99 <talmale.vastuk5566@gmail.com>
2020-07-26 10:24:02 +0530

    Updates

---
+++ precache-manifest.80f3ec95cd1c8ed570fc6884245fc284.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2689c9597c7f47f5cf21aac23321fa87",
+    "url": "/Better-Calc-React-App/index.html"
+  },
+  {
+    "revision": "675d4b1585901f0d7443",
+    "url": "/Better-Calc-React-App/static/css/main.e32bea30.chunk.css"
+  },
+  {
+    "revision": "dbff9c363beb665b8aa1",
+    "url": "/Better-Calc-React-App/static/js/2.e9f688c5.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/Better-Calc-React-App/static/js/2.e9f688c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "675d4b1585901f0d7443",
+    "url": "/Better-Calc-React-App/static/js/main.f491597d.chunk.js"
+  },
+  {
+    "revision": "d2fd2e586ac8e80d26ca",
+    "url": "/Better-Calc-React-App/static/js/runtime-main.5789092d.js"
+  }
+]);
